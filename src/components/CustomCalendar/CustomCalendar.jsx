d31fdc6b7cc56ce8e0f7a2be506cd5f6a311dc86
Grigorii Kokotkin <padgamerrfp@gmail.com>
2025-08-17 22:35:03 +0400

    added cancel, reschdule, complete btns

--- src/components/CustomCalendar/CustomCalendar.jsx
+++ src/components/CustomCalendar/CustomCalendar.jsx
@@ -9,12 +9,116 @@ const PIXELS_PER_HOUR = 60; // масштаб — 1 час = 60px
 const PIXELS_PER_MINUTE = PIXELS_PER_HOUR / 60;
 
 export default function CustomCalendar({ lessons }) {
+
     const [weekStart, setWeekStart] = useState(dayjs().startOf("week"));
+    const [selectedLesson, setSelectedLesson] = useState(null); // выбранный урок
+    const [notification, setNotification] = useState(null);
+    const [rescheduleMode, setRescheduleMode] = useState(false);
+    const [newDate, setNewDate] = useState("");
+    const [newStart, setNewStart] = useState("");
+    const [newEnd, setNewEnd] = useState("");
+
+    const ApiKey = import.meta.env.VITE_API_KEY;
 
     const days = Array.from({ length: 7 }, (_, i) => weekStart.add(i, "day"));
 
     const nextWeek = () => setWeekStart(prev => prev.add(1, "week"));
     const prevWeek = () => setWeekStart(prev => prev.subtract(1, "week"));
+
+    const handleDelete = (lesson) => {
+        fetch(`${ApiKey}/lessons/cancel`, {
+            method: "DELETE",
+            body: JSON.stringify({
+                lessonId: lesson.lessonId,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+            .then(r => {
+                if (r.ok) {
+                    setNotification("Урок удалён ✅");
+                    // через 1 сек обновим страницу
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 1000);
+                } else {
+                    setNotification("Ошибка при удалении ❌");
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                setNotification("Ошибка при удалении ❌");
+            });
+
+        setSelectedLesson(null);
+    };
+
+    const openReschedule = (lesson) => {
+        // Заполняем новые значения из урока
+        setNewDate(dayjs(lesson.date).format("YYYY-MM-DD"));
+        setNewStart(dayjs(lesson.startTime, "HH:mm:ss").format("HH:mm:ss"));
+        setNewEnd(dayjs(lesson.endTime, "HH:mm:ss").format("HH:mm:ss"));
+
+
+        setRescheduleMode(true);
+    };
+
+    const handleReschedule = () => {
+        console.log("Переносим:", selectedLesson.id, {
+            newDate,
+            newStart,
+            newEnd
+        });
+
+        fetch(`${ApiKey}/lessons/reschedule`, {
+            method: "POST",
+            body: JSON.stringify({
+                "lessonId" : selectedLesson.lessonId,
+                "date" : newDate,
+                "startTime" : newStart,
+                "endTime" : newEnd,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then(r => r.json())
+            .then(json => console.log(json))
+            .then(() => {
+                setNotification("Урок был перенесен ✅");
+                setTimeout(() => {
+                    window.location.reload();
+                }, 1000);
+            });
+        setSelectedLesson(null);
+
+        setRescheduleMode(false);
+        setSelectedLesson(null);
+        setNewDate("");
+        setNewStart("");
+        setNewEnd("");
+    };
+
+    const handleComplete = (lesson) => {
+        fetch(`${ApiKey}/lessons/change-status`, {
+            method: "POST",
+            body: JSON.stringify({
+                "lessonId" : lesson.lessonId,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then(r => r.json())
+            .then(json => console.log(json))
+            .then(() => {
+                setNotification("Урок отмечен пройденым ✅");
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 1000);
+            });
+         setSelectedLesson(null);
+    };
+
 
     return (
         <div className="calendar">
@@ -71,15 +175,93 @@ export default function CustomCalendar({ lessons }) {
                                             top: `${minutesFromTop * PIXELS_PER_MINUTE}px`,
                                             height: `${lessonDuration * PIXELS_PER_MINUTE}px`
                                         }}
-                                        onClick={() => console.log("Open LessonCard", l)}
+                                        onClick={() => setSelectedLesson(l)}
                                     >
                                         Урок
                                     </div>
                                 );
                             })}
                     </div>
+
+
                 ))}
             </div>
+
+            {/* Модалка */}
+            {selectedLesson && (
+                <div className="modal-overlay" onClick={() => setSelectedLesson(null)}>
+                    <div className="modal" onClick={(e) => e.stopPropagation()}>
+                        <h3 className="lesson-modal-info">Урок</h3>
+                        <p className="lesson-modal-info">{selectedLesson.date}</p>
+                        <p className="lesson-modal-info"> {selectedLesson.startTime} - {selectedLesson.endTime}</p>
+                        <p className="lesson-modal-info">Ученик {selectedLesson.studentName}</p>
+                        <p className="lesson-modal-info">Урок был? {selectedLesson.isCompleted === true ? "Да" : "Нет"}</p>
+                        <div className="modal-actions">
+                            <button className="delete-btn" onClick={() => handleDelete(selectedLesson)}>Удалить</button>
+                            <button className="reschedule-btn" onClick={() => openReschedule(selectedLesson)}>Перенести</button>
+                            <button className="complete-btn" onClick={() => handleComplete(selectedLesson)}>Завершить</button>
+                        </div>
+                        <button className="close-btn" onClick={() => setSelectedLesson(null)}>Закрыть</button>
+                    </div>
+                </div>
+            )}
+
+            {/* Уведомление */}
+            {notification && (
+                <div className="notification">
+                    {notification}
+                </div>
+            )}
+
+            {/* Второе модальное окно (перенос) */}
+            {/* Второе модальное окно (перенос) */}
+            {selectedLesson && rescheduleMode && (
+                <div className="modal-overlay" onClick={() => setRescheduleMode(false)}>
+                    <div className="modal" onClick={(e) => e.stopPropagation()}>
+                        <h3 className="lesson-modal-info">Перенести урок {selectedLesson.id}</h3>
+
+                        <label className="lesson-modal-info">
+                            Дата:
+                            <input
+                                className="lesson-modal-info"
+                                type="date"
+                                value={newDate}
+                                onChange={e => setNewDate(e.target.value)}
+                            />
+                        </label>
+
+                        <label className="lesson-modal-info">
+                            Время начала:
+                            <input
+                                className="lesson-modal-info"
+                                type="time"
+                                step="1"
+                                value={newStart}
+                                onChange={e => setNewStart(e.target.value)}
+                            />
+                        </label>
+
+                        <label className="lesson-modal-info">
+                            Время конца:
+                            <input className="lesson-modal-info"
+                                type="time"
+                                step="1"
+                                value={newEnd}
+                                onChange={e => setNewEnd(e.target.value)}
+                            />
+                        </label>
+
+                        <div className="modal-actions">
+                            <button className="reschedule-btn" onClick={handleReschedule}>
+                                Сохранить
+                            </button>
+                            <button className="close-btn" onClick={() => setRescheduleMode(false)}>
+                                Отмена
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
